a3278091a2368307fad220473ab08b07ed41e9e3
Rashed967 <rashedhasan967@gmail.com>
2023-06-13 00:41:56 +0600

    dashboard secure done

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ app.use(cors())
 app.use(express.json())
 
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://artCraftUser:pLOaqQOkVZW24t7t@cluster0.kt6fwyn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DbUser}:${process.env.DbPass}@cluster0.kt6fwyn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -135,6 +153,41 @@ async function run() {
     const result = await userCollection.updateOne(filter, updateDoc, options);
     res.send(result)
   });
+
+  app.post('/jwt', (req, res) => {
+    const user = req.body;
+    // console.log(user)
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30h' })
+
+    res.send({ token })
+  })
+
+
+  // check admin 
+
+  app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+    const email = req.params.email;
+
+    if (req.decoded.email !== email) {
+      res.send({ admin: false })
+    }
+
+    const query = { email: email }
+    const user = await userCollection.findOne(query);
+    const result = { admin: user?.roll === 'admin' }
+    res.send(result);
+  })
+
+
+  app.get('/getUserRoll', async(req, res) => {
+    const email = req.query.email
+    console.log(email)
+  if(!email){
+    res.send([])
+  }
+    const result = await userCollection.findOne({email})
+    res.send(result)
+})
 
 
     // Send a ping to confirm a successful connection
